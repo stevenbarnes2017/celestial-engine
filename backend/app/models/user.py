488927c3d3ed6bcdb04957b2a623867b8e4dfaab
Steven <stevenbarnes50@gmail.com>
2026-05-22 09:46:14 -0600

    db: expand password hash

--- backend/app/models/user.py
+++ backend/app/models/user.py
@@ -6,7 +6,7 @@ class User(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password_hash = db.Column(db.String(128), nullable=False)
+    password_hash = db.Column(db.String(255), nullable=False)
     billing_status = db.Column(db.String(20), default='free_beta')
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
 
